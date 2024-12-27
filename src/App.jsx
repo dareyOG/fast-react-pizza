2dd3a48aa91b7a4c81615f28d48f3e36193ceddf
Oludire Damilare <oludire@gmail.com>
2024-12-27 21:22:05 +0100

    ✅🏆 FEAT: load, connect and get menu data from loader using react-router loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router';
 
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: '/', element: <Home /> },
-      { path: '/menu', element: <Menu /> },
+      { path: '/menu', element: <Menu />, loader: menuLoader },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
       { path: '/order/:orderId', element: <Order /> }
@@ -23,15 +23,19 @@ const router = createBrowserRouter([
 function App() {
   return <RouterProvider router={router} />;
 
-  // return (
-  //   <BrowserRouter>
-  //     <Routes>
-  //       <Route path="/" element={<Home />} />
-  //       <Route path="/menu" element={<Menu />} />
-  //       <Route path="/cart" element={<Cart />} />
-  //     </Routes>
-  //   </BrowserRouter>
-  // );
+  /*   return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order/new" element={<CreateOrder />} />
+          <Route path="/order/:orderId" element={<Order />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  ); */
 }
 
 export default App;
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,26 @@
+import { useLoaderData } from 'react-router';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
+
 function Menu() {
-  return <h1>Menu</h1>;
+  // get menu data from loader
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {menu.map(pizza => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// load menu data
+export async function loader() {
+  const menu = await getMenu();
+
+  return menu;
 }
 
 export default Menu;
